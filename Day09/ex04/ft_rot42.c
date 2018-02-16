6d60e166bc9705af4e947c3b4bee6a4d64db4a98
Killian CAUSSE <kcausse@e3r7p15.42.fr>
2018-02-16 02:58:00 +0100

    Add ft_collatz_conjecture

--- Day09/ex04/ft_rot42.c
+++ Day09/ex04/ft_rot42.c
@@ -10,7 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int		is_letter(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+int		letter_loops_back(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c > 'i');
+	return (c > 'I');
+}
+
+int		get_new_letter(char c)
+{
+	if (c > 'i')
+		return ('a' + 16 + (c - 'z'));
+	return ('A' + 16 + (c - 'Z'));
+}
+
 char	*ft_rot42(char *str)
 {
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (is_letter(str[i]))
+		{
+			if (letter_loops_back(str[i]))
+				str[i] = get_new_letter(str[i]);
+			else
+				str[i] += 16;
+		}
+		i++;
+	}
 	return (str);
 }
--- Day09/ex07/ft_collatz_conjecture.c
+++ Day09/ex07/ft_collatz_conjecture.c
@@ -10,7 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int ft_collatz_conjecture(unsigned int base)
+unsigned int	ft_collatz_conjecture(unsigned int base)
 {
+	static int i;
 
+	i++;
+	if (base == 1)
+		return (i - 1);
+	if (base & 1)
+		return (ft_collatz_conjecture(base * 3 + 1));
+	return (ft_collatz_conjecture(base / 2));
 }
