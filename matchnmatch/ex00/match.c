43ca4f19578785698358c2197fb46a5627baf9bd
Killian CAUSSE <kcausse@e3r8p10.42.fr>
2018-02-18 17:23:14 +0100

    commit sudoku

--- matchnmatch/ex00/match.c
+++ matchnmatch/ex00/match.c
@@ -12,13 +12,13 @@
 
 int	match(char *s1, char *s2)
 {
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 	if (*s1 == *s2 && *s1 != '\0')
 		return (match(s1 + 1, s2 + 1));
 	if (*s1 != '\0' && *s2 == '*')
 		return (match(s1 + 1, s2) || match(s1, s2 + 1));
 	if (*s1 == '\0' && *s2 == '*')
 		return (match(s1, s2 + 1));
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
 	return (0);
 }
