efa7bb5fcecc347092323a7850c8dd97727e4e2e
Killian CAUSSE <kcausse@e3r8p10.42.fr>
2018-02-18 19:29:24 +0100

    1

--- matchnmatch/ex00/match.c
+++ matchnmatch/ex00/match.c
@@ -14,7 +14,7 @@ int	match(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == *s2 && *s1 != '\0')
+	if (*s1 == *s2)
 		return (match(s1 + 1, s2 + 1));
 	if (*s1 != '\0' && *s2 == '*')
 		return (match(s1 + 1, s2) || match(s1, s2 + 1));
--- matchnmatch/ex01/nmatch.c
+++ matchnmatch/ex01/nmatch.c
@@ -13,11 +13,11 @@
 int	nmatch(char *s1, char *s2)
 {
 	if (*s1 == *s2 && *s1 != '\0')
-		return (match(s1 + 1, s2 + 1));
+		return (nmatch(s1 + 1, s2 + 1));
 	if (*s1 != '\0' && *s2 == '*')
-		return (match(s1 + 1, s2) + match(s1, s2 + 1));
+		return (nmatch(s1 + 1, s2) + nmatch(s1, s2 + 1));
 	if (*s1 == '\0' && *s2 == '*')
-		return (match(s1, s2 + 1));
+		return (nmatch(s1, s2 + 1));
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	return (0);
