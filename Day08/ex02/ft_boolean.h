7a724436e25aee17d5242fd0448ae3a7c48b8ab9
Killian CAUSSE <kcausse@e3r12p4.42.fr>
2018-02-22 03:51:20 +0100

    Attempt to fix ft_boolean

--- Day08/ex02/ft_boolean.h
+++ Day08/ex02/ft_boolean.h
@@ -13,7 +13,7 @@
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 # include <unistd.h>
-# define EVEN(nbr) (nbr & 1)
+# define EVEN(nbr) (nbr % 2)
 # define TRUE (1)
 # define FALSE (0)
 # define EVEN_MSG "I have an even number of arguments.\n"
