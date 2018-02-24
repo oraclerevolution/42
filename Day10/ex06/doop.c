a7f2cff58eff0a2510b705c8641cfec31b9fa651
Killian CAUSSE <kcausse@e3r5p7.42.fr>
2018-02-24 22:56:01 +0100

    Add do-op

---
+++ Day10/ex06/doop.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do-op.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcausse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/24 19:50:02 by kcausse           #+#    #+#             */
+/*   Updated: 2018/02/24 19:50:02 by kcausse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		print(char *str);
+int		ft_putnbr(int nb);
+int		ft_atoi(char *str);
+
+void	get_op_and_nbr(char **av, int *nbr1, int *nbr2, char *operator)
+{
+	*nbr1 = ft_atoi(av[1]);
+	*nbr2 = ft_atoi(av[3]);
+	*operator = av[2][0];
+}
+
+int		main(int argc, char **argv)
+{
+	int		nbr[2];
+	char	operator;
+
+	if (argc != 4)
+		return (0);
+	get_op_and_nbr(argv, &nbr[0], &nbr[1], &operator);
+	if (operator == '-')
+		return (ft_putnbr(nbr[0] - nbr[1]));
+	if (operator == '+')
+		return (ft_putnbr(nbr[0] + nbr[1]));
+	if (operator == '/')
+	{
+		if (nbr[0] == 0 || nbr[1] == 0)
+			return (print("Stop : division by zero\n"));
+		return (ft_putnbr(nbr[0] / nbr[1]));
+	}
+	if (operator == '%')
+	{
+		if (nbr[0] == 0 || nbr[1] == 0)
+			return (print("Stop : modulo by zero\n"));
+		return (ft_putnbr(nbr[0] / nbr[1]));
+	}
+	return (ft_putnbr(0));
+}
--- Day10/ex06/utils.c
+++ Day10/ex06/utils.c
@@ -1,16 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kcausse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/09 01:04:41 by kcausse           #+#    #+#             */
-/*   Updated: 2018/02/09 01:04:41 by kcausse          ###   ########.fr       */
+/*   Created: 2018/02/24 20:35:07 by kcausse           #+#    #+#             */
+/*   Updated: 2018/02/24 20:35:09 by kcausse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *str)
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		ft_putnbr(int nb)
+{
+	int overflow;
+
+	overflow = 0;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		if (nb == -2147483648)
+		{
+			nb = 2147483647;
+			overflow = 1;
+		}
+		else
+			nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0' + overflow);
+	return (0);
+}
+
+int		ft_atoi(char *str)
 {
 	int pos;
 	int i;
@@ -28,4 +57,15 @@ int	ft_atoi(char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 		result = result * 10 + str[i++] - '0';
 	return (result * pos);
+}
+
+int		print(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+	return (0);
 }
