36f1e1b58315155ac328676d181831968b320216
Killian CAUSSE <kcausse@e3r8p6.42.fr>
2018-02-26 20:39:37 +0100

    Optimize ft_ultimate_range.c, add exam02 stuff

--- exam02/ft_strlen/ft_strlen.c
+++ exam02/ft_strlen/ft_strlen.c
@@ -1,26 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kcausse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/10 10:26:09 by kcausse           #+#    #+#             */
-/*   Updated: 2018/02/10 10:26:09 by kcausse          ###   ########.fr       */
+/*   Created: 2018/02/26 20:26:03 by kcausse           #+#    #+#             */
+/*   Updated: 2018/02/26 20:26:03 by kcausse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdlib.h>
-
-int	ft_ultimate_range(int **range, int min, int max)
-{
-	int i;
-
-	*range = NULL;
-	i = -1;
-	if (min >= max || (*range = (int*)malloc(sizeof(int) * (max - min))) == NULL)
-		return (0);
-	while (++i < (max - min))
-		(*range)[i] = i + min;
-	return (i);
-}
