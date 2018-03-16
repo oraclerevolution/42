a467787b541a351ae7079c56b9da8fd4c4569317
kilo <kilowwwww@gmail.com>
2018-03-16 17:41:01 +0100

    Make ft_print_combn noticeably faster and remove workaround for 9 characters-long numbers

--- Day02/ex07/ft_print_combn.c
+++ Day02/ex07/ft_print_combn.c
@@ -12,42 +12,9 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c);
-
-int		ft_putstr(void)
-{
-	int		i;
-	char	*str;
-
-	str = "012345678, 012345679, 012345689, 012345789, 012346789, \
-	012356789, 012456789, 013456789, 023456789, 123456789";
-	i = -1;
-	while (str[++i])
-		if (str[i] != '\t')
-			ft_putchar(str[i]);
-	return (-1);
-}
-
 int g_t;
 
-void	print_nbr_array(int *n, int size)
-{
-	int i;
-
-	i = 0;
-	while (++i < size)
-		if (i != 0 && n[i] <= n[i - 1])
-			return ;
-	i = 0;
-	if (g_t == 1)
-	{
-		ft_putchar(',');
-		ft_putchar(' ');
-	}
-	g_t = 1;
-	while (i < size)
-		ft_putchar('0' + n[i++]);
-}
+void	ft_putchar(char c);
 
 int		get_max(int size)
 {
@@ -55,15 +22,13 @@ int		get_max(int size)
 	int reverse;
 	int n;
 
-	if (size == 9)
-		return (ft_putstr());
 	g_t = 0;
 	reverse = 0;
 	n = 9;
 	max = 0;
 	while (size-- != 0)
 		reverse = reverse * 10 + n--;
-	while (reverse > 0)
+	while (reverse != 0)
 	{
 		max = max * 10 + reverse % 10;
 		reverse /= 10;
@@ -71,30 +36,62 @@ int		get_max(int size)
 	return (max + 1);
 }
 
+int		get_min(int size)
+{
+	int min;
+	int n;
+
+	g_t = 0;
+	n = 0;
+	min = 0;
+	while (size-- != 0)
+		min = min * 10 + n++;
+	return (min - 1);
+}
+
+void	print_nbr_array(int *n, int size)
+{
+	int i;
+
+	i = 0;
+	while (++i < size)
+		if (n[i] <= n[i - 1])
+			return ;
+	if (g_t == 1)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+	else
+		g_t = 1;
+	i = 0;
+	while (i < size)
+		ft_putchar('0' + n[i++]);
+}
+
 void	ft_print_combn(int n)
 {
-	int max[2];
+	int max;
 	int i;
 	int i2;
-	int i3;
+	int tmp;
 	int nbrs[n];
 
-	if (n <= 0 || n > 9 || (max[0] = get_max(n)) == -1)
+	if (n <= 0 || n > 9 || (max = get_max(n)) == -1)
 		return ;
-	i = -1;
-	while (++i < max[0])
+	i = get_min(n);
+	while (++i < max)
 	{
-		i2 = -1;
-		while (++i2 < n)
+		i2 = n;
+		tmp = i;
+		while (i2-- != 0)
 		{
-			max[1] = i;
-			i3 = i2;
-			while (++i3 < n)
-				max[1] /= 10;
-			nbrs[i2] = max[1] % 10;
-			if (i2 > 0 && nbrs[i2 - 1] >= nbrs[i2])
+			nbrs[i2] = tmp % 10;
+			if (i2 != n - 1 && nbrs[i2] >= nbrs[i2 + 1])
 				break ;
+			tmp /= 10;
 		}
-		print_nbr_array(nbrs, n);
+		if (i2 == -1)
+			print_nbr_array(nbrs, n);
 	}
 }
