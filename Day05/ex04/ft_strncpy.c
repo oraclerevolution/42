ec54d8c099ad4590feeb4144ce682860a90d9f16
kilo <kilowwwww@gmail.com>
2018-03-17 19:16:55 +0100

    Fix strncpy warning

--- Day05/ex04/ft_strncpy.c
+++ Day05/ex04/ft_strncpy.c
@@ -14,10 +14,15 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	i = -1;
-	while (src[++i] != '\0' && i < n)
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
 		dest[i] = src[i];
+		i++;
+	}
 	while (i < n)
+	{
 		dest[i++] = '\0';
+	}
 	return (dest);
 }
