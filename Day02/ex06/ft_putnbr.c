36da3dc1ef003a58012c1e5b0b737619aade434b
kilo <kilowwwww@gmail.com>
2018-03-17 19:12:48 +0100

    Remove useless variable

--- Day02/ex06/ft_putnbr.c
+++ Day02/ex06/ft_putnbr.c
@@ -14,23 +14,20 @@ void	ft_putchar(char c);
 
 void	ft_putnbr(int nb)
 {
-	int overflow;
-
-	overflow = 0;
 	if (nb < 0)
 	{
 		ft_putchar('-');
 		if (nb == -2147483648)
 		{
-			nb = 2147483647;
-			overflow = 1;
+			ft_putchar('2');
+			ft_putnbr(147483648);
+			return ;
 		}
-		else
-			nb = -nb;
+		nb = -nb;
 	}
 	if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
 	}
-	ft_putchar((nb % 10) + '0' + overflow);
+	ft_putchar((nb % 10) + '0');
 }
