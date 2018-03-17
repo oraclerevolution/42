fe0ec3ac6fcde2f44ec848e66e16d9594aaa82ba
kilo <kilowwwww@gmail.com>
2018-03-17 01:58:56 +0100

    Fix rev_wstr

--- exam_final/rev_wstr/rev_wstr.c
+++ exam_final/rev_wstr/rev_wstr.c
@@ -13,8 +13,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int g_dirty;
-
 char is_separator(char c)
 {
 	return (c == ' ' || c == '\t');
@@ -27,7 +25,7 @@ int get_word_start(char *str, int end)
 	return (end);
 }
 
-void print_trim_spaces(char *str, int len)
+void print_trim_spaces(char *str)
 {
 	int start;
 	int i;
@@ -35,18 +33,16 @@ void print_trim_spaces(char *str, int len)
 	start = 0;
 	while (is_separator(str[start]))
 		start++;
-	while (len >= 0 && str[len] > 0 && is_separator(str[len]) && (g_dirty = 1) == 1)
-		len--;
-	i = -1;
-	while (++i < len)
-		if(str[i] >= 32)
-			write(1, str + start + i, 1);
+	i = start;
+	while (str[start + i] != '\0' && !is_separator(str[start + i]))
+		i++;
+	write(1, str + start, i);
 }
 
 int main(int argc, char **argv)
 {
 	int i;
-	int i2;
+	int start;
 
 	if (argc != 2)
 	{
@@ -54,25 +50,21 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	i = 0;
-	g_dirty = 0;
-	while (argv[1][i])
+	while (argv[1][i] != '\0')
 		i++;
-	while (i-- > 0)
+	while (--i > 0)
 	{
-		if (is_separator(argv[1][i]))
-			continue;
-		i2 = get_word_start(argv[1], i);
-		print_trim_spaces(argv[1] + i2, i - i2 + 1);
-		i = i2;
-		if (i - 1 > 0)
-			write(1, " ", 1);
+		if (!is_separator(argv[1][i]))
+		{
+			start = get_word_start(argv[1], i);
+			print_trim_spaces(argv[1] + start);
+			i = start;
+			while (start != 0 && !is_separator(argv[1][start]))
+				start--;
+			if (is_separator(argv[1][start]))
+				write(1, " ", 1);
+		}
 	}
-	if (g_dirty)
-	{
-		while (!is_separator(argv[1][i]))
-			i++;
-		write(1, argv[1] + i - 1, 1);
-	}
-	write(1, "\n", 1);
+	write(1, "\n", 2);
 	return (0);
 }
